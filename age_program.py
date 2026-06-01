d5b34ee196715231de0757e56da56059037aadb0
Ash☣ <123337781+neonwisp@users.noreply.github.com>
2026-06-01 11:33:07 -0400

    Initial Python practice programs

---
+++ age_program.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+age = input("How old are you?")
+print("Hello,", name)
+print("You are", age, "years old!")
---
+++ favorite_food.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+food = input("What is your favorite food?")
+
+print("Hello,", name)
+print(food, "sounds delicious!")
