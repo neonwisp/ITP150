c67820ceea6b9652a1719709f28f05f16e7df940
Ash☣ <123337781+neonwisp@users.noreply.github.com>
2026-06-09 10:20:44 -0400

    Code for June 9th Lecture

---
+++ module1.py
@@ -0,0 +1,7 @@
+""" 
+Date Created: June 9, 2026
+Review of print() function and string concatenation in class
+
+"""
+print("Today is Monday") # edit 1, changed monday to Monday
+print("Today", "is", "Monday", sep="...") # edit 2, changed monday to Monday
