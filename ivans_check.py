553cdb50ab0508db2e6cca20e3b7a7a1fbf0a272
Ash☣ <123337781+neonwisp@users.noreply.github.com>
2026-06-24 22:48:01 -0400

    added ivans check from module 2  discussion post

---
+++ ivans_check.py
@@ -0,0 +1,34 @@
+""" 
+Author: Ashlee Raya
+Date Created: 6/19/2026
+double checking a student's answer to a question in our 
+Module 2: Discussion - PythonExpressions.
+
+The question was: Q6. Which of the following expressions...
+
+2*4
+2**4
+2.*4
+2**4.
+2/4
+2//4
+-2/4
+-2//4
+2%4
+2%-4
+
+will give you these outputs:
+
+-2
+16.0
+0.5
+"""
+
+import math
+
+# comparing 2**4 and 2**4. (Ivan's answer)
+result = 2**4
+check = 2**4.
+
+print(result)
+print(check)
